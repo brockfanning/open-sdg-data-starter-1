9e00efefe31194f1dddcbd36b202ae5c1044ba29
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2019-09-25 14:07:44 +0100

    Remove old info

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,11 +1,3 @@
-"""
-Created on Sun May 13 2018
-@author: dashton
-This is the parent script for building the data outputs. It loads the
-raw data from csv and sends it through the various processors to
-output the main data, edges, and headline in csv and json format.
-"""
-
 import output
 
 output.opensdg_output.execute()
